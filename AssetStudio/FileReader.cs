c7c4c9f3de800434371ab55561dccbaa40ccf2e6
Hashblen <hashanulblennon@gmail.com>
2024-07-05 19:46:32 +0200

    Fix assets missing

--- AssetStudio/FileReader.cs
+++ AssetStudio/FileReader.cs
@@ -251,7 +251,12 @@ namespace AssetStudio
                 catch (Exception) { }
                 reader.Position = 0;
             }
-
+            
+            if (reader.FileType == FileType.MhyFile && game.Type.IsZZZCB2())
+            {
+                reader.FileType = FileType.BlockFile;
+            }
+            
             Logger.Verbose("No preprocessing is needed");
             return reader;
         }
